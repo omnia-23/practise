d43df3444ab3c23c36e48a91ba0c0cf92391412c
omnia-23 <omnia-@example.com>
2022-08-15 21:26:10 +0200

    Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className ="navbar">
+        <div>Logo</div>
+        <div className='content'>
+            <div className='con'>Home</div>
+            <div className='con'>Content</div>
+            <div className='con'>About</div>
+        </div>
+    </div>
+  )
+}
